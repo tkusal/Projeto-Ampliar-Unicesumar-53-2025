039263af9e60992c9371390bfc21efb9bbb4b01f
tkusal <thiago.kusal@outlook.com>
2025-08-26 22:08:31 -0300

    Aula 26/08/2025

--- Semana 4/Ex3 - PARA CASA.c
+++ Semana 4/Ex3 - PARA CASA.c
@@ -2,3 +2,32 @@
     Escreva um programa que leia 5 números e imprima-os em ordem crescente.
     - User vetor para armazenar os 5 números!!!
 */
+
+#include <stdio.h>
+
+int main() {
+    int numeros[5];
+    int aux;
+
+    for (int i = 0; i < 5; i++) {
+        printf("Informe um número: ");
+        scanf("%d", &numeros[i]);
+    }
+
+    //BUBBLE SORT
+    for (int i = 0; i < 5; i++) {
+        for ( int j = 0 ; j < 5-1-i ; j++ ) {
+            if (numeros[j] > numeros[j+1]) {
+                aux = numeros[j];
+                numeros[j] = numeros[j+1];
+                numeros[j+1] = aux;
+            }
+        }    
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d  ", numeros[i]);
+    }
+
+    return 0;
+}
