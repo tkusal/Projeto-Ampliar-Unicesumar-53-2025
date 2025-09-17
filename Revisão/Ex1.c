c9ba20ad519bccb5204e647306ba1372ddefb3db
tkusal <thiago.kusal@outlook.com>
2025-09-16 21:44:49 -0300

    Aula 16/19/2025

---
+++ Revisão/Ex1.c
@@ -0,0 +1,10 @@
+/*
+A sequência de Collatz é uma sequência matemática que começa com um número inteiro positivo e 
+gera os termos seguintes usando duas regras simples: dividir por dois se o número for par, ou multiplicar por três e somar 
+um se for ímpar. A conjectura, proposta pelo matemático alemão Lothar Collatz, afirma que, independentemente do número inicial 
+escolhido, a sequência sempre terminará no ciclo 4, 2, 1. 
+
+Faça um programa em C que solicite um numerico inteiro decimal ao usuário.
+A partir deste número, gere a sequência de Collatz até chegar em 1.
+
+*/
